a173a3decc86be02310d825bb225c27e150f4eda
Dinescu Stefan Cristian <aaetherengine@gmail.com>
2016-01-31 13:19:23 +0200

    started implementing fbx sdk

---
+++ fbx_manager/fbxmanager.cpp
@@ -0,0 +1,6 @@
+#include "fbxmanager.hpp"
+
+FBXManager::FBXManager(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ fbx_manager/fbxmanager.hpp
@@ -0,0 +1,17 @@
+#ifndef FBXMANAGER_H
+#define FBXMANAGER_H
+
+#include <QObject>
+
+class FBXManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit FBXManager(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // FBXMANAGER_H
